ac0c0429e5f9cdd47430a4578a92e04106199d03
AhmedElaidy <alaidy.y123@gmail.com>
2024-05-01 00:16:20 +0300

    added get messages controller

--- backend/controllers/message.controller.ts
+++ backend/controllers/message.controller.ts
@@ -35,3 +35,25 @@ export const sendMessage = async (req: any, res: any) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const getMessages = async (req: any, res: any) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, userToChatId],
+      },
+    }).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
