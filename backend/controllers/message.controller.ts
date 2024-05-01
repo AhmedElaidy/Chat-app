967cafcba8200a1868e7cd8b11d5ed7c51bd55fc
AhmedElaidy <alaidy.y123@gmail.com>
2024-05-01 12:42:13 +0300

    using console.error instead of console.log in catchs

--- backend/controllers/message.controller.ts
+++ backend/controllers/message.controller.ts
@@ -31,7 +31,7 @@ export const sendMessage = async (req: any, res: any) => {
     await Promise.all([conversation.save(), newMessage.save()]);
     res.status(201).json(newMessage);
   } catch (error) {
-    console.log("Error in sendMessage controller: ", error.message);
+    console.error("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -53,7 +53,7 @@ export const getMessages = async (req: any, res: any) => {
 
     res.status(200).json(messages);
   } catch (error) {
-    console.log("Error in getMessages controller: ", error.message);
+    console.error("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
