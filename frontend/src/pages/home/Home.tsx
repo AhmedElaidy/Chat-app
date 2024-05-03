6650bf681662e98d24981b192ef5f6182d2d1a1a
AhmedElaidy <alaidy.y123@gmail.com>
2024-05-03 16:20:55 +0300

    added sidebar ( search Input, conversations , logout button )

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,5 +1,10 @@
-function home() {
-  return <div>home</div>;
-}
+import Sidebar from "../../components/sidebar/Sidebar";
 
-export default home;
+export default function Home() {
+  return (
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar />
+      {/* <MessageContainer /> */}
+    </div>
+  );
+}
