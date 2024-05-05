f3a85baafd47930cde6a7b3cf3017cba78d5194b
AhmedElaidy <alaidy.y123@gmail.com>
2024-05-05 21:12:20 +0300

    added (Messages, Message, MessageContainer ) components and finished UI

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,10 +1,11 @@
+import MessagesContainer from "../../components/messages/MessagesContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+    <div className="flex h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
-      {/* <MessageContainer /> */}
+      <MessagesContainer />
     </div>
   );
 }
