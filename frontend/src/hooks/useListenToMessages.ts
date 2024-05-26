79042729abc3ac0a75488fc5b32800c81b18b037
AhmedElaidy <alaidy.y123@gmail.com>
2024-05-26 15:21:50 +0300

    application done

--- frontend/src/hooks/useListenToMessages.ts
+++ frontend/src/hooks/useListenToMessages.ts
@@ -1,13 +1,17 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
-
+import whats_notification from "../assets/sounds/whats_notification.mp3";
 export default function useListenToMessages() {
   const { socket } = useSocketContext();
   const { messages, setMessages } = useConversation();
   useEffect(() => {
+    console.log("inside useEffect of messages");
     socket?.on("newMessage", (newMessage) => {
+      console.log("inside soket of new messages");
       newMessage.shouldShake = true;
+      const sound = new Audio(whats_notification);
+      sound.play();
       setMessages(
         messages && messages?.length > 0
           ? [...messages, newMessage]
@@ -15,6 +19,7 @@ export default function useListenToMessages() {
       );
     });
     return () => {
+      console.log("inside return of useEffect of messages");
       socket?.off("newMessage");
     };
   }, [messages]);
